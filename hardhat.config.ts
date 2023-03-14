5bd607ae95d6eb8bcbf90d04a5c1ac7e4ba3b8cf
Ebrahim Elbagory <ebrahim@umee.cc>
2023-03-14 12:40:14 -0700

    chore: configure hardhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,62 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-contract-sizer";
+import "@primitivefi/hardhat-dodoc";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+  defaultNetwork: "hardhat",
+
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.9",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 999999,
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        url: process.env.ETHEREUM_RPC || "",
+        blockNumber: 16828207, // block pinning gives x20 perfromance due to caching as stated on hardhat docs
+        enabled: true,
+      },
+    },
+    mainnet: {
+      url: process.env.ETHEREUM_RPC || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  dodoc: {
+    runOnCompile: false,
+    debugMode: true,
+    include: ["contracts/UmeeAxelarToken.sol"],
+    // More options...
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+  },
+
+  etherscan: {
+    apiKey: {
+      1: process.env.ETHERSCAN_API_KEY || "",
+    },
+  },
+  contractSizer: {
+    alphaSort: true,
+    disambiguatePaths: false,
+    runOnCompile: true,
+    strict: true,
+  },
 };
 
 export default config;
